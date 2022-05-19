6e6df98c83e443d8874e38280342c909e7668fed
bogoreh <bogoreh026@gmail.com>
2022-05-19 22:19:13 +0300

    The first commit

---
+++ 025_Music store app reactjs/public/src/components/layout/Header.js
@@ -0,0 +1,10 @@
+// Header component
+export default () => {
+  return (
+    <div className="title">
+      <span className="caption">
+        Simple <strong>Music App</strong>
+      </span>
+    </div>
+  )
+}
